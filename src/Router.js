024999db2bce6b6d95bb9fa483468160845d1b45
cunhaluisg5 <cunhaluisg5@gmail.com>
2019-09-25 14:01:08 -0300

    Alterado os botões inferiores da tela principal

--- src/Router.js
+++ src/Router.js
@@ -6,21 +6,44 @@ import Info from './pages/Info'
 import Donate from './pages/Donate'
 import BloodDonation from './pages/BloodDonation'
 
+import React from 'react';
 import { createAppContainer } from 'react-navigation';
 import { createStackNavigator } from 'react-navigation-stack';
 import { createBottomTabNavigator } from 'react-navigation-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome5';
+
 
 const AppTabNavigator = createBottomTabNavigator(
   {
     'Notificações': {
       screen: Info,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) =>
+          <Icon name="info" color={tintColor} size={15} />
+      },
     },
     'Doação': {
       screen: Donate,
+      navigationOptions: {
+        tabBarIcon: ({ tintColor }) =>
+          <Icon name="donate" color={tintColor} size={15} />
+      }
     },
   },
   {
     defaultNavigationOptions: {
+      tabBarOptions: {
+        activeTintColor: "#00008B",
+        inactiveTintColor: "#000000",
+        activeBackgroundColor: "#6495ED",
+        inactiveBackgroundColor: "#A9A9A9",
+        style: {
+          height: 50,
+        },
+        labelStyle: {
+          fontSize: 15,
+        },
+      },
     }
   }
 );
