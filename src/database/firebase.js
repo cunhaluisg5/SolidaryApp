8606721fb9e7a0e690caebfd671c4ce4ff05b309
cunhaluisg5 <cunhaluisg5@gmail.com>
2019-11-11 18:48:08 -0300

    Alterado o cadastro

--- src/database/firebase.js
+++ src/database/firebase.js
@@ -1,16 +1,16 @@
 import * as firebase from 'firebase';
 import firestore from 'firebase/firestore'
 
-const firebaseConfig = {
-    apiKey: "AIzaSyBakqxDNA7-axrdhxvL8qGxe8fNkWCTwac",
-    authDomain: "solidaryapp-eb90e.firebaseapp.com",
-    databaseURL: "https://solidaryapp-eb90e.firebaseio.com",
-    projectId: "solidaryapp-eb90e",
-    storageBucket: "solidaryapp-eb90e.appspot.com",
-    messagingSenderId: "947194087571",
-    appId: "1:947194087571:web:c3fa8b7119e49848d26566",
-    measurementId: "G-W5039FLLFC"
-};
-firebase.initializeApp(firebaseConfig);
+var firebaseConfig = {
+    apiKey: "AIzaSyASYCAcpMSBEUt_5CKg0qRj2llH5LRaiuk",
+    authDomain: "cadastro-6cf90.firebaseapp.com",
+    databaseURL: "https://cadastro-6cf90.firebaseio.com",
+    projectId: "cadastro-6cf90",
+    storageBucket: "cadastro-6cf90.appspot.com",
+    messagingSenderId: "542064520936",
+    appId: "1:542064520936:web:5513be07877e31bd51a35d"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
 
 export default firebase;
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -2,29 +2,19 @@ import React from 'react';
 import { StyleSheet, TextInput, View, Text, ScrollView } from 'react-native';
 import { TextInputMask } from 'react-native-masked-text'
 import firebase from '../database/firebase'
-import { Button, CheckBox, Card } from 'react-native-elements';
+import { Button, Card } from 'react-native-elements';
 import Icon from 'react-native-vector-icons/FontAwesome';
 
 class Register extends React.Component {
     constructor(props) {
         super(props);
         this.state = {
-            checkedPerson: true,
-            checkedOrganization: false,
-            persona: {
-                cpf: '',
-                nome: '',
-                dataNascimento: '',
-                cep: '',
-                endereco: '',
-                uf: '',
-                cidade: '',
-                telefone: '',
-                email: '',
-                usuario: '',
-                senha: ''
-            },
-            Ong: []
+            cpf: '',
+            nome: '',
+            telefone: '',
+            email: '',
+            usuario: '',
+            senha: ''
         }
     }
 
@@ -44,55 +34,26 @@ class Register extends React.Component {
     }
 
     tryRegister() {
-        if (this.state.checkedPerson) {
-            const ref = firebase.firestore().collection('Voluntario');
-            ref.add({
-                CPF: this.state.persona.cpf,
-                //nome: this.state.nome,
-                //dataNasc: this.state.dataNascimento, 
-                //cep: this.state.cep,
-                //endereco: this.state.endereco,
-                //uf: this.state.uf,
-                //cidade: this.state.cidade,
-                //telefone: this.state.telefone,
-                //email: this.state.email,
-                //usuario: this.state.usuario,
-                //senha: this.state.senha
+        const ref = firebase.firestore().collection('Voluntario');
+        ref.add({
+            CPF: this.state.cpf,
+            nome: this.state.nome,
+            telefone: this.state.telefone,
+            email: this.state.email,
+            usuario: this.state.usuario,
+            senha: this.state.senha
 
-            }).then(() => {
-                this.props.navigation.navigate("Main");
-            }).catch((error) => {
-                console.log("Erro ao adicionar o doc");
-            });
+        }).then((docRef) => {
+            this.props.navigation.navigate("Main");
+        }).catch((error) => {
+            console.log("Erro ao adicionar o doc");
+        });
 
-            console.log("Valor CPF: ", this.state.persona.nome, "valor Nome: ", this.state.persona.cpf)
-        } else {
-            const ref = firebase.firestore().collection('Ong');
-            ref.add({
-                name: this.state.name,
-                desc: this.state.desc,
-                img: this.state.img
-            }).then(() => {
-                this.props.navigation.navigate("Main");
-            }).catch((error) => {
-                console.log("Erro ao adicionar o doc");
-            });
-        }
-    }
-
-    checkPerson() {
-        this.setState({ checkedPerson: true, checkedOrganization: false })
-    }
-
-    checkOrganization() {
-        this.setState({ checkedOrganization: true, checkedPerson: false })
+        console.log("Valor CPF: ", this.state.nome, "valor Nome: ", this.state.cpf)
     }
 
     renderScreen() {
-        if (this.state.checkedPerson) {
-            return this.renderRegisterPerson()
-        }
-        return this.renderRegisterOrganization()
+        return this.renderRegisterPerson()
     }
 
     renderRegisterPerson() {
@@ -101,95 +62,38 @@ class Register extends React.Component {
                 <Text>CPF: </Text>
                 <TextInputMask style={styles.textInput}
                     type={'cpf'}
-                    value={this.state.persona.cpf}
-                    onChangeText={(text) => { this.setState({ persona: { cpf: text } }) }}
+                    value={this.state.cpf}
+                    onChangeText={(text) => { this.setState({ cpf: text }) }}
                     placeholder="999.999.999-99" />
                 <Text>Nome Completo: </Text>
                 <TextInput style={styles.textInput}
                     placeholder="nome ...."
-                    onChangeText={(value) => { this.setState({ persona: { nome: value } }) }} />
-                <Text>Data de Nascimento: </Text>
-                <TextInputMask style={styles.textInput}
-                    value={this.state.persona.dataNascimento}
-                    type={"datetime"}
-                    options={{ format: 'DD/MM/YYYY' }}
-                    value={this.state.persona.dataNascimento}
-                    placeholder="99/99/9999"
-                    onChangeText={(text) => this.setState({ persona: { dataNascimento: text } })} />
-                <Text>CEP: </Text>
-                <TextInput style={styles.textInput}
-                    //type={'zip-code'}
-                    placeholder="99.999-99"
-                    onChangeText={(value) => this.setState({ persona: { cep: value } })} />
-                <Text>Endereço: </Text>
-                <TextInput style={styles.textInput}
-                    placeholder="Rua ... AV.."
-                    onChangeText={(value) => this.setState({ persona: { endereco: value } })} />
-                <Text>UF: </Text>
-                <TextInput style={styles.textInput}
-                    placeholder="uf"
-                    onChangeText={(value) => this.setState({ persona: { uf: value } })} />
-                <Text>Cidade: </Text>
-                <TextInput style={styles.textInput}
-                    placeholder="cidade"
-                    onChangeText={(value) => this.setState({ persona: { cidade: value } })} />
+                    onChangeText={(value) => { this.setState({ nome: value }) }} />
                 <Text>Telefone: </Text>
                 <TextInput style={styles.textInput}
                     placeholder="telefone"
-                    onChangeText={(value) => this.setState({ persona: { telefone: value } })} />
+                    onChangeText={(value) => this.setState({ telefone: value })} />
                 <Text>E-mail: </Text>
                 <TextInput style={styles.textInput}
                     placeholder="user@email.com"
-                    onChangeText={(value) => this.setState({ persona: { email: value } })} />
+                    onChangeText={(value) => this.setState({ email: value })} />
                 <Text>Usuário: </Text>
                 <TextInput style={styles.textInput}
                     placeholder="user"
-                    onChangeText={(value) => this.setState({ persona: { usuario: value } })} />
+                    onChangeText={(value) => this.setState({ usuario: value })} />
                 <Text>Senha: </Text>
                 <TextInput secureTextEntry style={styles.textInput}
                     placeholder="pass"
-                    onChangeText={(value) => this.setState({ persona: { senha: value } })} />
+                    onChangeText={(value) => this.setState({ senha: value })} />
             </Card>
         )
-    }
-
-    renderRegisterOrganization() {
-        return <Card title="Cadastro de Instituição">
-            <Text>CNPJ: </Text>
-            <TextInput />
-            <Text>Nome: </Text>
-            <TextInput />
-            <Text>Responsável: </Text>
-            <TextInput />
-            <Text>Site: </Text>
-            <TextInput />
-            <Text>Login: </Text>
-            <TextInput />
-            <Text>Senha: </Text>
-            <TextInput />
-            <input type="submit" value="Cadastrar" />
-        </Card>
     }
 
     render() {
         return (
             <ScrollView style={styles.container}>
                 <View style={styles.containerText}>
-                    <Text style={styles.text}>Preencha os dados abaixo</Text>
-                    <Text style={styles.text}>para efetuar o cadastro</Text>
-                </View>
-
-                <View style={styles.checkbox}>
-                    <CheckBox
-                        title='Pessoa'
-                        checked={this.state.checkedPerson}
-                        onPress={() => this.checkPerson()}
-                    />
-                    <CheckBox
-                        title='Instituição'
-                        checked={this.state.checkedOrganization}
-                        onPress={() => this.checkOrganization()}
-                    />
+                    <Text style={styles.text}>Preencha os dados abaixo para efetuar o cadastro</Text>
                 </View>
 
                 <View>
